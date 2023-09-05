49c8b44d2d9953c97a849f295baa9cfb93908422
muhammadisa226 <muhammadisa226@gmail.com>
2023-09-05 09:57:37 +0700

    all done

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,81 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container" style="margin-top: 20px;">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Simple TodoList App</h5>
+        <small class="mt-3">Total Todo : {{ totalTODO }}</small>
+        <div class="row">
+          <div class="col-10">
+            <input type="text" class="form-control " v-model="todo" @keyup.enter="add">
+          </div>
+          <div class="col-2">
+            <button class="btn btn-outline-success" @click="add">Add</button>
+          </div>
+        </div>
+        <!-- List Todolist -->
+       <List
+          :todos="todos"
+          @deleteTodo="deleteTodo"
+          @doneTodo="doneTodo"
+        />
+        <!-- End List Todolist -->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      </div>
     </div>
-  </header>
+  </div>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script>
+import List from './components/List.vue';
+export default {
+  name: 'App',
+  components: {
+    List
+  },
+  data() {
+    return {
+      todo: "",
+      todos: []
+    }
+  },
+  mounted() {
+    this.todos = JSON.parse(localStorage.getItem('todos'))
+  },
+  methods: {
+    add() {
+      this.todos.unshift({
+        activity: this.todo,
+        isDone: false
+      })
+      this.todo = ''
+      this.saveToLocalStorage()
+    },
+    deleteTodo(todoIndex) {
+      this.todos = this.todos.filter((item, index) => {
+        if (index != todoIndex) {
+          return item
+        }
+      })
+      this.saveToLocalStorage()
+    },
+    doneTodo(todoIndex) {
+      this.todos = this.todos.filter((item, index) => {
+        if (index == todoIndex) {
+          item.isDone = true
+        }
+        return item;
+      })
+      this.saveToLocalStorage()
+    },
+    saveToLocalStorage() {
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+    }
+  },
+  computed: {
+    totalTODO() {
+      return this.todos.length
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
-</style>
+</script>
---
+++ src/components/List.vue
@@ -0,0 +1,44 @@
+<template>
+   <div class="mt-3">
+          <ul class="list-group">
+            <li class="list-group-item mt-2 " v-for="(todo,index) in todos" :key="index">
+            <div class="row">
+              <div class="col">
+                <span v-if="todo.isDone">
+                 <del>{{ todo.activity }}</del>
+                </span>
+                <span v-else>{{ todo.activity }}</span>
+              </div>
+              <div class="col-auto">
+                  <div class="row gx-2">
+                    <div class="col">
+                      <button @click="doneTodo(index)" class="btn btn-outline-info">Done</button>
+                    </div>
+                    <div class="col">
+                      <button @click="deleteTodo(index)" class="btn btn-outline-danger">Delete</button>
+                    </div>
+                  </div>
+              </div>
+            </div>
+            </li>
+          </ul>
+        </div>
+</template>
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      default: []
+    }
+  },
+  methods: {
+    deleteTodo(index) {
+      this.$emit('deleteTodo', index)
+    },
+    doneTodo(index) {
+      this.$emit('doneTodo', index)
+    }
+  }
+}
+</script>
